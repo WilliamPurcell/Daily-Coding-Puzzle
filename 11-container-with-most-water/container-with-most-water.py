248de972595c616be6c24bfbf040b12153cb3c22
William Purcell <67236645+WilliamPurcell@users.noreply.github.com>
2024-04-18 02:57:24 -0400

    Time: 513 ms (81.35%) | Memory: 22.2 MB (27.02%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -4,8 +4,7 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
-        if len(height)==2:
-            return min(height[0],height[1])
+
         left=0
         right = len(height)-1
         maxwater= abs(left-right)* min(height[left],height[right])
@@ -14,10 +13,10 @@ class Solution(object):
                 left +=1
             else :
                 right-=1
-            width=abs(left-right)
-            wallheight=min(height[left],height[right])
-            if maxwater<width*wallheight:
-                maxwater= width*wallheight
+            #width=abs(left-right)
+            #wallheight=min(height[left],height[right])
+            if maxwater<abs(left-right)*min(height[left],height[right]):
+                maxwater= abs(left-right)*min(height[left],height[right])
         return maxwater
 
         
